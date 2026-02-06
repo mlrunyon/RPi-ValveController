b19ff3a4bd30ce873f4d66373d53b711c158e94b
Maddy Runyon <mlr2199@columbia.edu>
2026-02-06 10:21:32 -0500

    second indentation edit

--- valvecontrol.py
+++ valvecontrol.py
@@ -55,7 +55,6 @@ valves = [
         'description': 'Turbo',
         'excluded': 2
     }
-]
 
 
 def parsecontrol(item, command):
@@ -216,8 +215,8 @@ def httpstatus():
         `description` (str), and its current `status` (bool or other relevant type
         returned by the `status` function).
     """
-   states = lib8mosind.get_all(STACK)  # bitmask of all channel states
-   return [
+    states = lib8mosind.get_all(STACK)  # bitmask of all channel states
+    return [
         {
             'id': valve['id'],
             'description': valve['description'],
